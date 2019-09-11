d31b988aaf28054bfee64a3005eb1639e00b7cbb
John <reelyard@protonmail.com>
2019-09-11 17:20:22 -0400

    Added GitHub image conversion to script.

--- migration-script.php
+++ migration-script.php
@@ -1,5 +1,5 @@
 <?php
-main('developers/file'); 
+main(); 
 
 // Call all other functions from here.
 function main($single_file = null) {
@@ -12,21 +12,21 @@ function main($single_file = null) {
         // Get list of files.
         $files = findFiles();
     }
-    
-    // Readme-ify internal links.
-    foreach($files as $file) {
-        replaceSlashesWithDashes($file);
-    }
 
-    // GitHub-ify images.
+    // // Internal links.
+    // foreach($files as $file) {
+    //     replaceSlashesWithDashes($file);
+    // }
+
+    // Images.
     foreach($files as $file) {
         hardcodeImagesToGitHub($file);
     }
 
-    // Readme-ify # sections.
-    foreach($files as $file) {
-        fixSectionLinks($file);
-    }
+    // // Sections.
+    // foreach($files as $file) {
+    //     fixSectionLinks($file);
+    // }
 }
 
 // Find and replace any slashes `/` with dashes `-` in links, but not images.
@@ -40,6 +40,7 @@ function replaceSlashesWithDashes($filename) {
 
 // Find all links that include a hash `#` and add `section-` directly after the hash `#`
 function fixSectionLinks($filename) {
+    // There is currently a bug in this regex where any link, internal or external, with a hash `#` will be caught. External links still work, but do no jump to the correct section when visited.
     $regex = '/(\[.*\])(\(.*\#)(.*\))/';
     $new_text = preg_replace($regex, '$1$2section-$3', file_get_contents($filename));
 
@@ -48,8 +49,12 @@ function fixSectionLinks($filename) {
 }
 
 // Change ![](/developers/...) links to GitHub stored images.
-function hardcodeImagesToGitHub() {
+function hardcodeImagesToGitHub($filename) {
+    $regex = '/(\!\[.*\])(\(\/developers)(.*\))/';
+    $new_text = preg_replace($regex, '$1(https://raw.githubusercontent.com/aionnetwork/docs/master/developers$3', file_get_contents($filename));
 
+    // Save and overwrite the markdown file.
+    file_put_contents($filename, $new_text);
 }
 
 // Find all markdown files within this repo.
